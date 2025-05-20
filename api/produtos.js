f4750c2c5653cd82b4af70c9fc75f69eeda640e5
Nicolas Sousa <nicollasouza141@gmail.com>
2025-05-20 10:28:51 -0300

    Alimentando Api

--- api/produtos.js
+++ api/produtos.js
@@ -12,14 +12,62 @@ export default function handler(req, res) {
       id: 1,
       imagem: "https://forusi-api.vercel.app/imagens/acabamento-para-registro.jpg",
       nome: "Acabamento Para Registro",
-      descricao: "Descrição do produto Acabamento Para Registro."
+      descricao: "Descrição do produto."
     },
     {
       id: 2,
       imagem: "https://forusi-api.vercel.app/imagens/cabide-duplo-curve-cromado.jpg",
       nome: "Cabide Duplo Curve Cromado",
-      descricao: "Descrição do produto Cabide Duplo Curve Cromado."
+      descricao: "Descrição do produto."
     }
-    // ... outros produtos
+    ,
+    {
+      id: 3,
+      imagem: "https://forusi-api.vercel.app/imagens/cabide-simples-curve-cromado.jpg",
+      nome: "Cabide Simples Curve Cromado",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 4,
+      imagem: "https://forusi-api.vercel.app/imagens/ducha-higienica-curve-com-derivacao.jpg",
+      nome: "Ducha Higiênica Curve com Derivação",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 5,
+      imagem: "https://forusi-api.vercel.app/imagens/ducha-higienica-sml.jpg",
+      nome: "Ducha Higiênica SML",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 6,
+      imagem: "https://forusi-api.vercel.app/imagens/kit-instalacao-ar-condicionado-3metros.jpg",
+      nome: "Kit Instalação Ar Condicionado 3 Metros",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 7,
+      imagem: "https://forusi-api.vercel.app/imagens/torneira-tanque.png",
+      nome: "Torneira Tanque",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 8,
+      imagem: "https://forusi-api.vercel.app/imagens/torneira-lavatorio-mesa-mega-line-black-fosco.jpg",
+      nome: "Torneira Lavatório Mesa Mega Line Black Fosco",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 9,
+      imagem: "https://forusi-api.vercel.app/imagens/torneira-lavatorio-bica-movel-mesa.png",
+      nome: "Kit Instalação Ar Condicionado 10 Metros",
+      descricao: "Descrição do produto."
+    },
+    {
+      id: 10,
+      imagem: "https://forusi-api.vercel.app/imagens/torneira-cozinha-filtro-parede-mega-line-c-arejador-articulavel-1.4-de-volta.png",
+      nome: "Torneira Cozinha Filtro Parede Mega Line c/ Arejador Articulável 1.4 de Volta",
+      descricao: "Descrição do produto."
+    }
   ]);
 }
